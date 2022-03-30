9050381554ab191df83aa071429048dffaaae380
BehrangHaghdoust <62517402+BehrangHaghdoust@users.noreply.github.com>
2022-03-30 21:46:25 +0430

    project header

---
+++ src/pages/Home.js
@@ -0,0 +1,20 @@
+
+const Home = () => {
+
+    return (
+        <div className="container mt-5">
+            <div className="row text-center">
+                <div className="col-md-12">
+                    <h2>Shopping Projcet</h2>
+                    <p className="mt-4">
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi minima magnam, ducimus delectus assumenda corporis molestias doloremque,
+                        asperiores aspernatur, odit eligendi molestiae expedita! Voluptate reprehenderit unde odio ratione accusamus voluptas consectetur vel aliquam beatae.
+                         Eaque impedit, accusantium aliquam, doloremque exercitationem nemo neque eius, cupiditate fugit harum nihil voluptate ad necessitatibus.
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
